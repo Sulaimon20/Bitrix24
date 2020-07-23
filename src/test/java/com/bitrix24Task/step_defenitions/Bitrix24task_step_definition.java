c018d04df175ae63a08c642ec0d266b14da7d6ee
Sulaimon20 <sulaimon_s@yahoo.com>
2020-07-23 16:22:40 -0400

    Bitrix24 task functionality testing

--- src/test/java/com/bitrix24Task/step_defenitions/Bitrix24task_step_definition.java
+++ src/test/java/com/bitrix24Task/step_defenitions/Bitrix24task_step_definition.java
@@ -25,6 +25,9 @@ public class Bitrix24task_step_definition {
         loginPage.login();
         String actualTitle="Portal";
         Assert.assertEquals(Driver.getDriver().getTitle(), actualTitle);
+
+
+
     }
 
 
@@ -38,9 +41,11 @@ public class Bitrix24task_step_definition {
     wait.until(ExpectedConditions.visibilityOf(bitrix24TaskPages.priorityBox)).click();
 
     }
+
     @When("User see fire icon colored")
     public void user_see_fire_icon_colored() {
 
+    Assert.assertTrue("the clicking of priority checkbox is failed",bitrix24TaskPages.priorityCheckBox.isSelected());
         //the fire icon should be verified
 
     }
@@ -50,13 +55,50 @@ public class Bitrix24task_step_definition {
     public void userClicksOnTheLetterAInTheBottomOfTheMessageBox() {
     wait.until(ExpectedConditions.visibilityOf(bitrix24TaskPages.visualEditor)).click();
 
+
     }
 
     @Then("The editor text-bar on the top of the message box is displayed")
     public void theEditorTextBarOnTheTopOfTheMessageBoxIsDisplayed() {
-    Assert.assertTrue("Element does not displayed",bitrix24TaskPages.postVisualEditor.isDisplayed());
+
+
+        Assert.assertTrue("Element does not displayed",bitrix24TaskPages.postVisualEditor.isDisplayed());
+
 
     }
 
+
+//Scenario #3
+
+    @Then("User writes in <Thing to do> box {string}")
+    public void user_writes_in_thing_to_do_box(String string) {
+
+    }
+
+
+    @Then("User writes in <Message body> box {string}")
+    public void user_writes_in_message_body_box(String string) {
+
+    }
+    @Then("User clicks on <upload files> icon")
+    public void user_clicks_on_upload_files_icon() {
+
+    }
+    @Then("User clicks <upload files and images> and upload file and pictures from local disks")
+    public void user_clicks_upload_files_and_images_and_upload_file_and_pictures_from_local_disks() {
+
+    }
+    @Then("User clicks <Send> button")
+    public void user_clicks_send_button() {
+
+    }
+    @Then("User clicks on <Task> button om menu bar")
+    public void user_clicks_on_task_button_om_menu_bar() {
+
+    }
+    @Then("User see the the uploaded files")
+    public void user_see_the_the_uploaded_files() {
+
+    }
 
 }
